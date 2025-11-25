9a75d89f9e309728ec61f906e54d0154d0869aee
ruhancmendes <ruhancmendes@gmail.com>
2025-11-25 17:51:00 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import "./App.css";
+
+import { useState } from "react"
+
+export default function App() {
+  const [name, setName] = useState("")
+
+  return (
+    <div>
+      <h1>Evento {name}</h1>
+
+      <form>
+        <input
+          type="text"
+          placeholder="Nome do evento"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <span className="error">Nome é obrigatório</span>
+
+        <input type="date" placeholder="Nome do evento" lang="pt-BR" />
+
+        <select defaultValue="">
+          <option value="" disabled>
+            Selecione...
+          </option>
+
+          <option value="technology">React</option>
+          <option value="entertainment">Node.js</option>
+          <option value="business">Javascript</option>
+          <option value="business">Typescript</option>
+        </select>
+
+        <textarea placeholder="Descrição" rows={4} />
+
+        <button type="submit">Salvar</button>
+      </form>
+    </div>
+  );
+}
